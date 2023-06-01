1350435ed2c134b1ab568e6c50823273d73f6bd7
emsqrc <95850318+emsqrc@users.noreply.github.com>
2023-06-01 13:36:53 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,32 @@
+# Токен вашего бота
+TOKEN = ''
+# устанавливаем параметры подключения
+USERNAMESSH = ''
+PASSWORDSSH = ''
+
+NMSLOGIN = ''
+NMSPASS = ''
+
+KASSALOGIN = ''
+KASSAPASS = ''
+# Приветствие
+HELLOMSG = 'Вас вітає NovaNet NMS v.0.3'
+#
+
+#
+BOTADMIN = 123456
+#
+WORKERNAME = {
+    
+}
+
+ADMINS = [BOTADMIN]
+
+#     1      2      3      4      5     6      7      8     9     10     11    12      13    14    15     16     17     18     19     20     21    22    23     24     25      26    27    28    29     30     31     32     33     34    35     36     37     38
+U = "true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true"
+#
+NOACCESSLIST = 'Вас немає в спискові доступу\nЗверніться до адміністратора'
+#
+IPA = ''
+USERA = ''
+PASSA = ''
