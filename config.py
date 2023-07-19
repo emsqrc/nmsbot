14c1a61531ef5e2e9ecc4da99f6cab67c6891b7b
emsqrc <95850318+emsqrc@users.noreply.github.com>
2023-07-19 12:40:11 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,45 +1,32 @@
 # Токен вашего бота
-TOKEN = '5323225784:AAHmokKvOySqqOOUUpks8N26frA-wLBNf5A'
+TOKEN = ''
 # устанавливаем параметры подключения
-USERNAMESSH = 'coul'
-PASSWORDSSH = 'gOSXlietyASyxiYE'
+USERNAMESSH = ''
+PASSWORDSSH = ''
 
-NMSLOGIN = 'support1'
-NMSPASS = 'a7L}pz5Nfn;e64mj'
+NMSLOGIN = ''
+NMSPASS = ''
 
-KASSALOGIN = 'msqrc'
-KASSAPASS = 'atmega8l'
+KASSALOGIN = ''
+KASSAPASS = ''
 # Приветствие
 HELLOMSG = 'Вас вітає NovaNet NMS v.0.3'
 #
 
 #
-BOTADMIN = 670724318
+BOTADMIN = 0000000
 #
 WORKERNAME = {
-    670724318: 'admin',
-    890091473: 'Ярослав малий',
-    1270352374: 'Андрій Паскал',
-    1295973954: 'Рома Оконченко',
-    629816898: 'Саша Селецький',
-    913120106: 'Ярослав Крижанівський',
-    2019346396: 'Женя Коваль',
-    598410764: 'Саша Драченко',
-    423758001: 'Ігор Гречко',
-    846158498: 'Яна бугалтер',
-    640388489: 'Олег Маланіч',
-    398845306: 'Влад Безноско',
-    511310866: 'Вова Похитайло;',
-    513011036: 'Володимр Миколайович'
+    000000: 'admin'
 }
 
-ADMINS = [BOTADMIN, 423758001, 629816898]
+ADMINS = [BOTADMIN, 000000, 1111111]
 
 #     1      2      3      4      5     6      7      8     9     10     11    12      13    14    15     16     17     18     19     20     21    22    23     24     25      26    27    28    29     30     31     32     33     34    35     36     37     38
 U = "true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true||true"
 #
 NOACCESSLIST = 'Вас немає в спискові доступу\nЗверніться до адміністратора'
 #
-IPA = '45.155.81.22'
-USERA = 'bot'
-PASSA = '06780179'
+IPA = ''
+USERA = ''
+PASSA = ''
